1347cd5f9b1576b82cc416908da0a812ba64b05d
Maik Basso <maik@maikbasso.com.br>
2019-12-11 10:51:49 -0300

    Fix scripts.

--- scripts/startup-raspi.sh
+++ scripts/startup-raspi.sh
@@ -2,7 +2,7 @@
 
 
 cd /home/ubuntu/catkin_ws/
-source devel/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
 
 # please, configure the drone id
 roslaunch multi_uav_xbee multi_uav_xbee_network.launch droneId:=0 recordBag:=1
