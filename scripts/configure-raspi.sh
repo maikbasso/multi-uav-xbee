191cdb8cfd45b8b27810c9ea571ba17f71dafc76
Maik Basso <maik@maikbasso.com.br>
2019-12-11 09:39:04 -0300

    Update scripts (1).

--- scripts/configure-raspi.sh
+++ scripts/configure-raspi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo sed -i 's/sh /home/$USER/catkin_ws/src/multi-uav-xbee/scripts/startup-raspi.sh &/exit 0/g' /etc/rc.local 
+sudo sed -i 's+sh /home/$USER/catkin_ws/src/multi-uav-xbee/scripts/startup-raspi.sh &+exit 0+g' /etc/rc.local 
 sudo echo "exit 0" >> /etc/rc.local
