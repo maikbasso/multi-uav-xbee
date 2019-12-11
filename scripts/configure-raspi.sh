b1959a506a087541003a295fefbb50dff2460ec2
Maik Basso <maik@maikbasso.com.br>
2019-12-11 11:57:35 -0300

    Fix mavros connection.

--- scripts/configure-raspi.sh
+++ scripts/configure-raspi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p /home/ubuntu/catkin_ws/src/multi-uav-xbee/bag
+
 sudo sed -i 's/exit/#exit/g' /etc/rc.local
+sudo sed -i 's/source/#source/g' /etc/rc.local
 sudo echo "source /home/ubuntu/catkin_ws/src/multi-uav-xbee/scripts/startup-raspi.sh &" >> /etc/rc.local
 sudo echo "exit 0" >> /etc/rc.local
